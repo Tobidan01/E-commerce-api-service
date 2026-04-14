e5eb485d2e4d1411cf5621e98f0245dce9c542ae
Tobidan01 <tobid6422@gmail.com>
2026-04-14 18:34:12 +0100

    admim

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <?php
 
+
+declare(strict_types=1);
+error_log("REQUEST: " . $_SERVER['REQUEST_METHOD'] . " " . $_SERVER['REQUEST_URI']);
 var_dump([
   'admin' => file_exists(__DIR__ . '/app/Services/AdminService.php'),
   'wishlist' => file_exists(__DIR__ . '/app/Services/WishlistService.php'),
 ]);
 
 die();
-declare(strict_types=1);
-error_log("REQUEST: " . $_SERVER['REQUEST_METHOD'] . " " . $_SERVER['REQUEST_URI']);
-
 require_once __DIR__ . '/vendor/autoload.php';
 
 use App\Core\Router;
